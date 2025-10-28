c1388a79f8afc9198f26ce0f124085af2808c87b
Dj-Mouse-cmd <arnaud.fradin.2021@gmail.com>
2025-10-28 14:19:12 +0000

    .

---
+++ more_functions_nested_loops/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code for more_numbers function
+ *
+ * Description: This program tests the more_numbers function,
+ * which should print 10 times the numbers from 0 to 14,
+ * each sequence followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
+
