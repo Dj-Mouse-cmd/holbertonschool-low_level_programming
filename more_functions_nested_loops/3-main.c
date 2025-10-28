f09193d62eca7f9b9adb7eea99c039427ef20315
Dj-Mouse-cmd <arnaud.fradin.2021@gmail.com>
2025-10-28 09:41:56 +0000

    .

---
+++ more_functions_nested_loops/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code for print_numbers function
+ *
+ * Description: This program tests the print_numbers function,
+ * which should print the numbers from 0 to 9 followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
+
