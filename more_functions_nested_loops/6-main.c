0d0471035a4e95b9cd4f8b2524bbe270dad70ba1
Dj-Mouse-cmd <arnaud.fradin.2021@gmail.com>
2025-10-28 14:26:56 +0000

    .

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code for print_line function
+ *
+ * Description: This program tests print_line with different values.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
+
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * Description: If n is 0 or less, the function prints only a new line.
+ * The line ends with a newline character.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
