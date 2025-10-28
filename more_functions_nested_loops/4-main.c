4f279eb115b0d2c87781b4ebe42197229b1a4b47
Dj-Mouse-cmd <arnaud.fradin.2021@gmail.com>
2025-10-28 10:53:36 +0000

    .

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code for print_most_numbers function
+ *
+ * Description: This program tests the print_most_numbers function,
+ * which should print the numbers from 0 to 9 except 2 and 4,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
+
